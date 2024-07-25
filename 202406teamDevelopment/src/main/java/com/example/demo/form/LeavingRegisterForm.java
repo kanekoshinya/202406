a05cfe4be71c0bc08606e7cb03fbc64c60b39e4a
makina0617 <f.nico.m.moon@gmail.com>
2024-07-26 01:50:02 +0900

    退勤登録修正（高橋）

--- 202406teamDevelopment/src/main/java/com/example/demo/form/LeavingRegisterForm.java
+++ 202406teamDevelopment/src/main/java/com/example/demo/form/LeavingRegisterForm.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -21,7 +22,7 @@ public class LeavingRegisterForm implements Serializable {
     private Integer user_id;
     
     // ステータス
-    @NotNull(message = "ステータスを入力してください")
+    @NotEmpty(message = "ステータスを入力してください")
     private String status;
     
     // 退勤日
